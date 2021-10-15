32d904b9e5d169ff1c26b2e48cec989ffcb05754
sylphiel88 <jihmels@gmail.com>
2021-10-15 11:42:57 +0200

    admin panel 1

--- src/components/Main/Administrator.js
+++ src/components/Main/Administrator.js
@@ -1,27 +1,35 @@
 import React, { useState, useEffect } from "react"
+import UserC from "./userC"
 
-function Administrator({props}){
-    const [curPage, setCurrPage] = useState(0)
-    
-    function closehandler(){
-        var id = "s"+curPage;
-        document.getElementById(id)
+function Administrator({ props }) {
+    const [curPage, setCurPage] = useState(0)
+
+    const closeHandler = async () => {
+        var id = "s" + curPage;
+        var anim = "spalte_anim" + curPage;
+        var spalte = "spalte"+curPage
+        document.getElementById(id).classList.replace(spalte,"spalte_big");
+        document.getElementById(id).classList.replace("spalte_anim",anim);
+        if(curPage===1){
+            document.getElementById("us").classList.replace("userSend","userSend2");
+        }
+        console.log(id, anim,document.getElementById(id).classList);
+        setTimeout(() => setCurPage(0), 1000);
     }
 
-
-    return(
+    return (
         <div>
-            <div className="zeile" style={{marginTop: "5vh"}}>
-                <div id="s1" className={curPage===1?"spalte1 spalte_anim":"spalte1"} onClick={() => setCurrPage(1)}>User-Kontrollzentrum</div>
-                <div id="s2" className={curPage===2?"spalte2 spalte_anim":"spalte2"} onClick={() => setCurrPage(2)}>b</div>
-                <div id="s3" className={curPage===3?"spalte3 spalte_anim":"spalte3"} onClick={() => setCurrPage(3)}>c</div>
+            <div className="zeile" style={{ marginTop: "5vh" }}>
+                <div id="s1" className={curPage === 1 ? "spalte1 spalte_anim" : "spalte1"} onClick={() => setCurPage(1)}>{curPage===1?<UserC/>:"User-Kontrollzentrum"}</div>
+                <div id="s2" className={curPage === 2 ? "spalte2 spalte_anim" : "spalte2"} onClick={() => setCurPage(2)}>b</div>
+                <div id="s3" className={curPage === 3 ? "spalte3 spalte_anim" : "spalte3"} onClick={() => setCurPage(3)}>c</div>
             </div>
             <div className="zeile">
-                <div id="s4" className={curPage===4?"spalte4 spalte_anim":"spalte4"} onClick={() => setCurrPage(4)}>d</div>
-                <div id="s5" className={curPage===5?"spalte5 spalte_anim":"spalte5"} onClick={() => setCurrPage(5)}>e</div>
-                <div id="s6" className={curPage===6?"spalte6 spalte_anim":"spalte6"} onClick={() => setCurrPage(6)}>f</div>
+                <div id="s4" className={curPage === 4 ? "spalte4 spalte_anim" : "spalte4"} onClick={() => setCurPage(4)}>d</div>
+                <div id="s5" className={curPage === 5 ? "spalte5 spalte_anim" : "spalte5"} onClick={() => setCurPage(5)}>e</div>
+                <div id="s6" className={curPage === 6 ? "spalte6 spalte_anim" : "spalte6"} onClick={() => setCurPage(6)}>f</div>
             </div>
-            {curPage!=0?<div class="close" onClick={closehandler()}>X</div>:""}
+            {curPage != 0 ? <div class="close" onClick={() => closeHandler()}>X</div> : ""}
         </div>
     )
 }
---
+++ src/components/Main/userC.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+
+function UserC({ props }) {
+
+    const [datar, dataChg] = useState([])
+    const [page, chgPage] = useState(1)
+    const [numPer, chgNumPer] = useState(9)
+    const [pages, setPages] = useState(1)
+
+    useEffect(async ()=>{
+        let getStr='http://localhost:5000/api/v1/user/notactivatedusers?page='+page+'&perPage='+numPer;
+        console.log(getStr);
+        axios.get(getStr,{headers: { 'content-type': 'application/json' }})
+            .then(response =>dataChg(response.data))
+        axios.get('http://localhost:5000/api/v1/user/getCountUsers?perPage='+numPer,{headers: { 'content-type': 'application/json' }})
+            .then(response =>setPages(response.data.pages))
+    },[])
+
+    function UserList(props){
+        const listItems = datar.map((user)=>
+            <div className="card carduser"><div className="card-divider carduserT">{user.username} not activated!</div><div className="card-section\"><input className="chkuser2" id={user.username} type="checkbox"></input><label className="chkuser" for={user.username}>Activate</label></div></div>
+        )
+        return <div>{listItems}</div>
+    }
+    return(
+        <div>
+            <UserList/>
+            {pages}
+            <input type="button" class="userSend" id="us" value="Absenden"></input>
+        </div>
+    )
+}
+
+export default UserC
